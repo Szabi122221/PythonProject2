40ff2b87c26b3aed285fa172e4f308ed80b2d11c
Szabi12221 <kelemenszabolcs3@outlook.hu>
2025-09-18 08:53:40 +0200

    2025.09.18_02

--- 03.py
+++ 03.py
@@ -1,15 +1,22 @@
-egyik_szam = input("Kérek egy számot")
-if egyik_szam.isdigit():
-    egyik_szam = int(egyik_szam)
-    if egyik_szam==0:
-        print("Nem megfelelő szám")
-    elif egyik_szam > 100:
-        print("Túl nagy számot adtál meg")
+def szam_bekerese(legnagyobb_szam):
+    szam = input("Kérek egy számot")
+    if szam.isdigit():
+        szam = int(szam)
+        if szam==0:
+            print("Nem megfelelő szám")
+        elif szam > legnagyobb_szam:
+            print("Túl nagy számot adtál meg")
+        else:
+            print("Megfelelő szám")
     else:
-        print("Megfelelő szám")
-else:
-    print("Nem számot adtál meg!")
+        print("Nem számot adtál meg!")
+    return szam
 
+# Program indítás
+egyik_szam = szam_bekerese(10)
+print(egyik_szam)
+masik_szam = szam_bekerese(100)
+print(masik_szam)
 
 
 # masik_szam = input("Kérek egy másik számot")

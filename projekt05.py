3b9b365db4a707a6dfba27aed475fff2dae263ad
Szabi12221 <kelemenszabolcs3@outlook.hu>
2025-10-02 09:48:28 +0200

    2025.10.02_01

---
+++ projekt05.py
@@ -0,0 +1,58 @@
+
+class Teglalap():
+    alakzat = "téglalap"
+    def __init__(self, alap = 8, magassag = 6):
+        self.alap = alap
+        self.magassag = magassag
+        self.adatok()
+
+    def adatok(self):
+        print(f"A {self.alakzat} alapja = {self.alap}")
+        print(f"A {self.alakzat} magassága = {self.magassag}")
+
+    def kerulet(self):
+        return 2 * self.alap + 2 * self.magassag
+
+    def terulet(self):
+        return self.alap * self.magassag
+
+class Negyzet(Teglalap):
+    alakzat = "négyzet"
+    def __init__(self, alap):
+        super().__init__()
+        self.alap = alap
+        self.magassag = alap
+
+    def adatok(self):
+        print(f"A {self.alakzat} alapja = {self.alap}")
+
+    def kerulet(self):
+        return 4 * self.alap
+
+    def terulet(self):
+        return self.alap * self.alap
+
+class Haromszog(Teglalap):
+    alakzat = "derékszögű háromszög"
+    def __init__(self, alap, magassag):
+        super().__init__()
+        self.alap = alap
+        self.magassag = magassag
+
+
+tegla = Teglalap()
+print(tegla.adatok())
+print("A kerülete= ", tegla.kerulet())
+print("A területe= ", tegla.terulet())
+print()
+
+negy = Negyzet(8)
+print(negy.adatok())
+print("A kerülete= ", negy.kerulet())
+print("A területe= ", negy.terulet())
+print()
+
+harom = Haromszog(8, 10)
+'''print(harom.adatok())
+print("A területe= ", harom.terulet())'''
+

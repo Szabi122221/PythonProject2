b5cc3fdfd6ab6e268f770091890a43611d98579b
Szabi12221 <kelemenszabolcs3@outlook.hu>
2025-10-30 10:23:27 +0100

    2025_10_30_2

--- projekt08_backend.py
+++ projekt08_backend.py
@@ -1,9 +1,11 @@
 from flask import Flask, jsonify
+import random
 
 app = Flask(__name__)
 @app.route("/api/data")
 def get_data():
-    return jsonify({"uzenet": "Udv a backendbol"})
+    szam = random.randint(1, 100)
+    return jsonify({"uzenet": f"{szam}"})
 if __name__ == '__main__':
     app.run(port=5000)
 
--- projekt08_frontend.py
+++ projekt08_frontend.py
@@ -1,8 +1,26 @@
 import projekt08_n
 import tkinter as tk
 from tkinter import messagebox
+import requests
 
+def adatkeres():
+    try:
+        valasz = requests.get("http://127.0.0.1:5000/api/data", timeout=3)
+        valasz.raise_for_status()
+        adat = valasz.json()
+        cimke.config(text=adat["uzenet"])
+        app.dobasok_szama_bemenet.set(adat["uzenet"])
+
+    except:
+        messagebox.showerror("Hiba", "Nem sikerült a kapcsolat!")
 
 root = tk.Tk()
 app = projekt08_n.KockaDobasMentes(root)
+
+api_gomb = tk.Button(root, text="Lekérés", command=adatkeres)
+api_gomb.grid(column=0, row=4, pady=10)
+
+cimke = tk.Label(root, text="...")
+cimke.grid(column=1, row=4, pady=10)
+
 root.mainloop()
